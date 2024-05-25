b19bffae829b11311513ee31490d770f48ea5942
Vitor Valentim <vitorvalentin840@gmail.com>
2024-05-25 19:07:24 -0300

    .

--- js/server.js
+++ js/server.js
@@ -1,10 +1,17 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
 
-server.use(middlewares);
-server.use(router);
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running')
-});
+    console.log('JSON Server is running')
+})
+
+module.exports = server
