3ea68f85d692dbee8db416daeb2adf1385ab871a
Vitor Valentim <vitorvalentin840@gmail.com>
2024-05-25 19:20:44 -0300

    .

--- js/server.js
+++ js/server.js
@@ -1,7 +1,14 @@
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+//const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
